4fd7b06bca2950d7eb3cb4fc652788156f4cb304
DanielaVeRo27 <2070067330101@ingenieria.usac.edu.gt>
2023-03-06 17:02:31 -0600

    3er Avance Proyecto1

--- Principal.py
+++ Principal.py
@@ -1,3 +1,7 @@
+from ArchivoXml import Lectura
+from Tablero import CrearTablero, Imprimir
+
+
 """
 ♣ → Verificaciones que dedo hacer o tomar en cuenta en el programa 
 ♥ → Correciones que debo de hacer 
@@ -11,27 +15,45 @@ opc = 0
 
 def  Menu():
     
-    global opc  
+    global opc , OrganismoList, MuestrasList, CeldasVivasList
+    
     print()
-    print("___♥___♥___MENU___♥___♥____")
-    print("| 1. Cargar Archivo       ♥")
-    print("♥ 2. Celdas por prosperar |")                # identifica las celdas donde las muestras pueden prosperar 
-    print("| 3. Colocar Muestra      ♥")
-    print("♥ 4. Inicialización       |")
-    print("| 5. Reporte final        ♥")
-    print("♥ 6. Salir                |")
-    print("|____♥___♥___♥___♥___♥____♥")    
+    print("♥___♥___♥___MENU___♥___♥____♥___")
+    print("| 1. Cargar Archivo            ♥")
+    print("♥ 2. Ver informacion Muestra   |")                # identifica las celdas donde las muestras pueden prosperar 
+    print("| 3. Colocar Muestra           ♥")
+    print("♥ 4. Inicialización            |")
+    print("| 5. Reporte final             ♥")
+    print("♥ 7. Graficar                  |")
+    print("♥          6. Salir            |")
+    print("|____♥___♥___♥___♥___♥____♥____♥")    
     print()
+    
     print("Ingrese La opcion que desea Realizar", end="    ")
     opc = input()
+    
     print()
 
     if opc == "1" :
         print("Lectura del Archivo XML")
+        print("Ingerese la ruta de su archivo: ", end="   ")
+        Listas = Lectura()
+        OrganismoList = Listas['Organismos']
+        MuestrasList = Listas['Muestras']
+        CeldasVivasList = Listas['CeldasVivas']
+           
         # ♥  PEDIR LA RUTA DEL ARCHIVO POR LEER 
 
     elif opc == "2":
-        print("Las Celdas en donde prosperan las muestras son las siguientes")
+        Datos = MuestrasList.Imprimir()
+        print()
+        print()
+        N_Totales = Datos['FilasT']
+        M_Totales = Datos['ColumnasT']
+        print("Ingrese el Id de la muestra de la que desea ver la informacion ", end= "     " )
+        ID_MuestrasList = input()
+        CeldasVivasList.infoCeldas(ID_MuestrasList)
+        #CeldasVivasList.BCeldasVivas(ID_MuestrasList, N_Totales,M_Totales)
     
     elif opc == "3":
         print("Usted a decidido colocar una muestra en la cuadricula: ")
@@ -53,11 +75,36 @@ def  Menu():
     elif opc == "4":
         # ♦ Preguntar si esta seguro de borrar los datos, si no lo esta mostrar un mensaje diciendo usted ha decidido no inicializar nada y luego volver a mostra menu 
         print("Usted ha inicializado el programa")
+        OrganismoList.Inicializar()   
+
 
     elif opc== "5":
         print("Reporte final de las actualizaciones de las celdas y muestras ") # ♠ aqui debe de ir el reporte xml con la info final 
+        OrganismoList.Imprimir()
+        CeldasVivasList.imprimir()
+        MuestrasList.Imprimir()
     elif opc == "6":
         print("usted ha salido ")
+    elif opc == "7":
+        print("Graficar Celdas")
+        print()
+        MuestrasList.MuestrasDisponibles()
+        print()
+        print("Ingrese el codigo de la muestra que desea ver:  ", end="  ")
+        Cod =input()
+        print()
+        Info = MuestrasList.InfoTablero(Cod)
+        F = Info['Filas']
+        C = Info['Columnas']
+        print(F,C)        
+        print("FIN PRUEBA")
+        CrearTablero(Cod,F,C)
+        print("sdjkhdih")
+        CeldasVivasList.LLenarTableroJ(Cod,int(F),int(C))
+        Imprimir()
+        
+        
+
     else:
         print("opcion erronea")
 if __name__ == '__main__':
